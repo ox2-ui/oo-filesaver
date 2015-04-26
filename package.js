8de32fb5109f6ea6b6ef1491fed470f0a168853b
Lauricio Su <me@lauricio.com>
2015-04-26 20:27:08 +0700

    initial commit

---
+++ package.js
@@ -0,0 +1,22 @@
+Package.describe({
+  name: 'ox2:filesaver',
+  summary: 'FileSaver.js library',
+  version: '1.0.0',
+  git: ' /* Fill me in! */ '
+});
+
+var S = 'server';
+var C = 'client';
+var CS = [C, S];
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.0.2.1');
+  api.addFiles('lib/FileSaver.js', C);
+  api.export('saveAs');
+});
+
+Package.onTest(function(api) {
+  api.use('tinytest');
+  api.use('ox2:filesaver');
+  api.addFiles('tests/oo-filesaver-tests.js');
+});
